7ae30eb2ad0d432eb02041bf1f54f478b3aef935
Hedgebzh <axel.gore35@gmail.com>
2023-02-12 17:52:09 +0100

    Create admin user Django, update models with relation between DB

--- ASGT/tournaments/admin.py
+++ ASGT/tournaments/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from tournaments.models import User, Games, Tournament
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'id', 'email', 'created_at')
+
+class GamesAdmin(admin.ModelAdmin):
+    list_display = ('game_name', 'id', 'created_at')
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('tournament_name', 'id', 'game', 'admin', 'nb_players', 'player1', 'player2',
+    'player3', 'player4', 'player5', 'player6', 'player7', 'player8', 'player9', 'created_at', 'updated_at',
+    'start_date', 'start_time', 'tournament_infos'
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Games, GamesAdmin)
+admin.site.register(Tournament, TournamentAdmin)
--- ASGT/tournaments/models.py
+++ ASGT/tournaments/models.py
@@ -5,11 +5,47 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Si nous souhaitez créer un objet (ou un champ) dans une BDD, toujours faire hériter de models.Model
 # tournaments/models.py
 
+class User(models.Model):
+    
+    def __str__(self):
+        return f'{self.username}' #permet d'afficher directement le username dans l'administration Django plutôt qu'un nom d'objet + id
+
+    username = models.fields.CharField(max_length=30)
+    created_at = models.fields.DateTimeField(
+        default=timezone.now(),
+        verbose_name='Créé le'
+        )
+    updated_at = models.fields.DateTimeField(
+        auto_now=True,
+        verbose_name='Modifié le'
+        )
+    email = models.fields.EmailField(
+        blank=True,
+        null=True
+        )
+    password = models.fields.CharField(
+        max_length=25,
+        blank=True,
+        null=True
+        )
+
 class Games(models.Model):
+
+    def __str__(self):
+        return f'{self.game_name}'
     
     game_name = models.fields.CharField(max_length=100)
+    created_at = models.fields.DateTimeField(
+        default=timezone.now(),
+        verbose_name = 'Ajouté le'
+        )
 
 class Tournament(models.Model):
+
+    
+
+    def __str__(self):
+        return f'{self.tournament_name}'
 
     class Games_choices(models.TextChoices):
         FIFA = 'FIFA'
@@ -22,7 +58,10 @@ class Tournament(models.Model):
         blank=True,
         null=True
         )
-    created_at = models.fields.DateTimeField(default=timezone.now()) 
+    created_at = models.fields.DateTimeField(
+        default=timezone.now(),
+        verbose_name="Créé le"
+        ) 
     updated_at = models.fields.DateTimeField(auto_now=True)
     nb_players = models.fields.IntegerField(validators=
         [MinValueValidator(8, "Le nombre de participants est imposé à 8 pour le moment"), 
@@ -50,7 +89,8 @@ class Tournament(models.Model):
     player2 = models.fields.CharField(
         verbose_name="Joueur 2",
         max_length=20,
-        default="Bot"
+        blank=True,
+        null=True
         )
     player3 = models.fields.CharField(
         verbose_name="Joueur 3",
@@ -88,6 +128,12 @@ class Tournament(models.Model):
         blank=True,
         null=True
         )
+    player9 = models.ForeignKey(
+        User, 
+        null=True, 
+        on_delete=models.SET_NULL, #set_null car si on utilise CASCADE ça supprimera tout le tournoi
+        verbose_name="Joueur 9"
+        )
     start_date = models.fields.DateField(
         verbose_name="Date de début",
         blank=True,
@@ -104,22 +150,4 @@ class Tournament(models.Model):
         )
 
 
-class User(models.Model):
-    user_name = models.fields.CharField(max_length=30)
-    created_at = models.fields.DateTimeField(default=timezone.now())
-    updated_at = models.fields.DateTimeField(auto_now=True)
-    tournament_id = models.fields.CharField(
-        max_length=20,
-        blank=True,
-        null=True
-        )
-    email = models.fields.EmailField(
-        "Saisissez une adresse courriel valide",
-        blank=True,
-        null=True
-        )
-    password = models.fields.CharField(
-        max_length=25,
-        blank=True,
-        null=True
-        )
+
