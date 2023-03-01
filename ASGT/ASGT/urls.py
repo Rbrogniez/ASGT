9cac8b2960484e48f0fe1447b948788ed829f643
Axel <axel.gore35@gmail.com>
2023-03-01 14:54:19 +0100

    Add logout possibility

--- ASGT/ASGT/urls.py
+++ ASGT/ASGT/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 #mettre toutes les urls des pages dans url patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accueil/', views.homepage),
+    path('accueil/', views.homepage, name='homepage'),
     path('a-propos/', views.about),
     path('tournois/', views.tournament, name='tournament-list'),
     path('tournois/<int:tournament_id>/', views.tournament_detail, name='tournament-detail'),
@@ -33,8 +33,8 @@ urlpatterns = [
     path('jeux/', views.games, name='tournament-game'),
     path('confirmation-contact/', views.contact_ok),
     path('tournois/<int:tournament_id>/supprimer/', views.tournament_delete, name='tournament-delete'),
-    path('login/', authentication.views.login_page, name='login')
-
+    path('login/', authentication.views.login_page, name='login'),
+    path('logout/', authentication.views.logout_user, name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ASGT/authentication/views.py
+++ ASGT/authentication/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from . import forms
+
+def logout_user(request):
+    logout(request)
+    return redirect('homepage')
 
 def login_page(request):
     form = forms.LoginForm()
     message = ''
     if request.method == 'POST':
-        form = forms.LoginForm(request.POSET)
+        form = forms.LoginForm(request.POST)
         if form.is_valid():
             user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             if user is not None:
@@ -16,3 +20,4 @@ def login_page(request):
                 message = 'Identifiants invalides.'
                 
     return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+
